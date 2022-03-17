d3739c8a46c95b28149aa0d9114b8a40fb54bda1
Ganning Xu <ganningxu@gmail.com>
2022-03-17 00:12:02 -0400

    translate screen still has small issue

--- components/TranslatorScreen.js
+++ components/TranslatorScreen.js
@@ -1,80 +1,217 @@
+// import { Icon } from "react-native-elements";
+// import { Button, Box, Text, Heading, HStack, Flex } from 'native-base';
+
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useState } from "react";
 import {
-    SafeAreaView,
-    View,
-    ImageBackground,
     StyleSheet,
-    Image,
+    Text,
+    View,
+    TouchableOpacity,
+    Dimensions,
+    SafeAreaView,
+    ImageBackground,
 } from "react-native";
-import React from "react";
+import { Camera } from "expo-camera";
+import { cameraWithTensors } from "@tensorflow/tfjs-react-native";
+import { loadLayersModel, ready, scalar, tidy } from "@tensorflow/tfjs";
+import { cropTensor } from "@teachablemachine/image/src/utils/tf";
 import { Icon } from "react-native-elements";
-import { Button, Box, Text, Heading, HStack, Flex } from 'native-base';
+
+
+// const MODEL_URL = "https://teachablemachine.withgoogle.com/models/OBWpGU-Yu/";
+const MODEL_URL = "https://teachablemachine.withgoogle.com/models/EYAXePbkj/";
+let transcriptRes = [];
+let push_status = false;
+let temp_letter;
+setInterval(() => {
+    push_status = true;
+}, 1000);
+
+const TensorCamera = cameraWithTensors(Camera);
+
 
 export default function TranslatorScreen({ navigation }) {
+    /*
+        the issue right now is that the app only makes predictions that are "unknown" when the machine learning model made predictions that used to be different letters. Changing the model to another teachable machine link only results in the same one-result prediction for another class.
+    */
+    const [permission, setPermission] = useState(false);
+    const [model, setModel] = useState(null);
+    const [metadata, setMetadata] = useState(null);
+    const [prediction, setPrediction] = useState(null);
+    const [transcript, setTranscript] = useState(null);
+
+    useEffect(() => {
+        (async () => {
+            await ready();
+            setModel(await loadLayersModel(MODEL_URL + "model.json"));
+            setMetadata(await (await fetch(MODEL_URL + "metadata.json")).json());
+            const { status } = await Camera.requestPermissionsAsync();
+            setPermission(status === "granted");
+        })();
+    }, []);
+
+    function readyCameraStream(images) {
+        const streamLoop = async () => {
+            const nextImageTensor = images.next().value;
+
+            const logits = tidy(() => {
+                const nextImageTensorCropped = cropTensor(nextImageTensor, false);
+                const batchedNextImage = nextImageTensorCropped.expandDims(0);
+
+                return model.predict(
+                    batchedNextImage.toFloat().div(scalar(127)).sub(scalar(1))
+                );
+            });
+
+            const data = await logits.data();
+
+            const prediction = [];
+            for (let i = 0; i < data.length; i++) {
+                prediction.push({
+                    className: metadata.labels[i],
+                    probability: data[i],
+                });
+            }
+
+            let max = prediction[0].probability;
+            let pred_letter = prediction[0].className;
+            for (let i = 1; i < prediction.length; ++i) {
+                if (prediction[i].probability > max) {
+                    max = prediction[i].probability;
+                    pred_letter = prediction[i].className;
+                }
+            }
+
+            if (push_status) {
+                console.log(pred_letter)
+                if (temp_letter == pred_letter && pred_letter != "unknown") {
+                    if (pred_letter === "space") {
+                        transcriptRes.push(" ");
+                    } else {
+                        transcriptRes.push(pred_letter);
+                    }
+                } else {
+                    console.log("temp:", temp_letter, "pred:", pred_letter);
+                    temp_letter = pred_letter;
+                }
+                push_status = false;
+            }
+
+            setPrediction(prediction);
+            setTranscript(transcriptRes);
+
+            requestAnimationFrame(streamLoop);
+        };
+        requestAnimationFrame(streamLoop);
+    }
+
+    if (permission === false || model == null || metadata == null)
+        return <Text>Loading...</Text>;
+
+    const image = require("../assets/home_bkg.png");
+
     return (
         <SafeAreaView style={styles.container}>
-            <Box
-                pt="10"
-                px="4"
-            >
-                <Flex direction="row" mt="6" justifyContent="center">
-                    <Image
-                        source={require("../assets/learn_rec.png")}
-                        style={styles.rec}
-                        alt="Alternate Text"
-                    />
-                    <Heading size="xl" textAlign="center" ml="4">SLTranslate
-                        <Heading size="xl" textAlign="center" color="#ffa36e"> Tutorial
-                        </Heading>
-                    </Heading>
-                </Flex>
-
-                <Box mt="4" bg='orange.100' px={4} py={4} rounded='md'>
-                    <Heading fontSize='lg'>
-                        How to use SLTranslate
-                    </Heading>
-                    <Text fontSize='sm'>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-                    </Text>
-                </Box>
-
-
-                <Box mt="4" bg='blue.100' px={4} py={4} rounded='md'>
-                    <Heading fontSize='lg'>
-                        Translate Screen
-                    </Heading>
-                    <Text fontSize='sm'>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-                    </Text>
-                </Box>
-
-                <Box mt="4" bg='green.100' px={4} py={4} rounded='md'>
-                    <Heading fontSize='lg'>
-                        Menu Screen
-                    </Heading>
-                    <Text fontSize='sm'>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-                    </Text>
-                </Box>
-            </Box>
-
-            <Box mt="4">
-                <Button
-                    onPress={() => navigation.navigate("Menu")}
-                    mx='auto'
-                    w='200px'
-                    bg="#ffa36e"
-                    rounded='lg'
+            <View style={{ flex: 1, flexDirection: "column" }}>
+                <View
+                    style={{
+                        flex: 2,
+                        flexDirection: "row",
+                        backgroundColor: "#FFFACA",
+                        justifyContent: "center",
+                    }}
                 >
+                    <View style={{ flex: 1, justifyContent: "center" }}>
+                        <TouchableOpacity onPress={() => navigation.navigate("Home")}>
+                            <Icon name="arrow-left" type="simple-line-icon" color="black" />
+                        </TouchableOpacity>
+                    </View>
+                    <View
+                        style={{ flex: 2, justifyContent: "center", alignItems: "center" }}
+                    >
+                        <Text
+                            style={{ fontSize: 24, fontFamily: "Avenir", fontWeight: "500" }}
+                        >
+                            Translate
+                        </Text>
+                    </View>
+                    <View style={{ flex: 1, justifyContent: "center" }}>
+                        <TouchableOpacity
+                            onPress={() => {
+                                transcriptRes = [];
+                            }}
+                        >
+                            <Icon name="trash" type="simple-line-icon" color="black" />
+                        </TouchableOpacity>
+                    </View>
+                </View>
 
-                    <Text fontSize='lg' fontWeight='bold' color='white'>
-                        Go Back
+                <View style={{ flex: 7 }}>
+                    <TensorCamera
+                        style={{
+                            ...styles.camera,
+                            zIndex: 1,
+                        }}
+                        type={Camera.Constants.Type.front}
+                        cameraTextureHeight={200}
+                        cameraTextureWidth={200}
+                        resizeHeight={224}
+                        resizeWidth={224}
+                        resizeDepth={3}
+                        autorender={false}
+                        onReady={readyCameraStream}
+                    >
+                        <View
+                            style={{
+                                width: Dimensions.get("window").width,
+                                height: Dimensions.get("window").height * 0.6,
+                            }}
+                        />
+                    </TensorCamera>
+                </View>
+
+                <View
+                    style={{
+                        flex: 1,
+                        backgroundColor: "#D1E1FF",
+                        borderTopLeftRadius: 20,
+                        borderTopRightRadius: 20,
+                        alignItems: "center",
+                        justifyContent: "center",
+                    }}
+                >
+                    <Text>
+                        {prediction
+                            ? prediction.reduce((mostLikely, current) =>
+                                mostLikely.probability < current.probability
+                                    ? current
+                                    : mostLikely
+                            ).className
+                            : "Loading prediction...."}
                     </Text>
-                </Button>
-            </Box>
+                </View>
 
+                <View style={{ flex: 3, backgroundColor: "#D1E1FF" }}>
+                    <View
+                        style={{
+                            flex: 1,
+                            backgroundColor: "#FFE3D3",
+                            borderTopLeftRadius: 20,
+                            borderTopRightRadius: 20,
+                            alignItems: "center",
+                            paddingTop: 20,
+                        }}
+                    >
+                        <Text>
+                            {transcript ? transcript.join("") : "Loading prediction...."}
+                        </Text>
+                    </View>
+                </View>
+            </View>
 
-
-        </SafeAreaView >
+            <StatusBar style="auto" />
+        </SafeAreaView>
     );
 }
 
@@ -82,20 +219,13 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
     },
     image: {
         flex: 1,
         justifyContent: "center",
         width: "100%",
         height: "100%",
-    },
-    roundButton: {
-        width: 100,
-        height: 100,
-        justifyContent: "center",
-        alignItems: "center",
-        padding: 10,
-        borderRadius: 100,
-        backgroundColor: "black",
     },
 });
